6040608bc9a376c7a1005c0cdf3d0f1dc1bfe03d
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-01-17 08:31:13 +0100

    day 9 optimizations

--- src/day09.rs
+++ src/day09.rs
@@ -1,37 +1,39 @@
+// use itertools::Itertools;
 use std::collections::HashSet;
-
-#[derive(Clone, PartialEq, Eq, Hash, Debug)]
-struct Coord {
-    x: i32,
-    y: i32,
-}
 
 pub fn solve() -> (i64, i64) {
     let buf = include_bytes!("../inputs/input09.txt");
-    let p1: i64 = simulate(buf, 2);
-    let p2: i64 = simulate(buf, 10);
-    (p1, p2)
-}
-
-fn simulate(buf: &[u8], num_knots: i32) -> i64 {
-    let mut rope: Vec<Coord> = vec![];
-    let mut visited: HashSet<Coord> = HashSet::new();
-
-    for _ in 0..num_knots {
-        rope.push(Coord { x: 0, y: 0 });
-    }
+    let mut instrs: Vec<(char, i32)> = vec![];
 
     for line in String::from_utf8_lossy(buf).trim().split("\n") {
         let mut words = line.split(' ');
         let dir: char = words.next().unwrap().chars().next().unwrap();
         let n = words.next().unwrap().parse::<i32>().unwrap();
+        instrs.push((dir, n));
+    }
 
-        for _ in 0..n {
-            move_head(&mut rope[0], dir);
+    let p1: i64 = simulate(&instrs, 2);
+    let p2: i64 = simulate(&instrs, 10);
+    (p1, p2)
+}
+
+fn simulate(instrs: &Vec<(char, i32)>, num_knots: i32) -> i64 {
+    let mut rope: Vec<(i32, i32)> = vec![];
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+
+    for _ in 0..num_knots {
+        rope.push((0, 0));
+    }
+
+    for (dir, n) in instrs {
+        for _ in 0..*n {
+            move_head(&mut rope[0], *dir);
 
             for i in 1..num_knots {
-                move_follow(&mut rope, i as usize);
+                let i0 = i as usize;
+                rope[i0] = move_follow(&rope[i0 - 1], &rope[i0]);
             }
+
             visited.insert(rope.last().unwrap().clone());
         }
     }
@@ -39,54 +41,37 @@ fn simulate(buf: &[u8], num_knots: i32) -> i64 {
     visited.len() as i64
 }
 
-fn move_head(head: &mut Coord, dir: char) {
+fn move_head(head: &mut (i32, i32), dir: char) {
     match dir {
-        'U' => head.y -= 1,
-        'D' => head.y += 1,
-        'L' => head.x -= 1,
-        'R' => head.x += 1,
+        'U' => head.1 -= 1,
+        'D' => head.1 += 1,
+        'L' => head.0 -= 1,
+        'R' => head.0 += 1,
         _ => panic!(),
     }
 }
 
-fn move_follow(rope: &mut Vec<Coord>, i: usize) {
-    let h = &rope[i - 1];
-    let t = &rope[i];
-
+fn move_follow(h: &(i32, i32), t: &(i32, i32)) -> (i32, i32) {
     if is_touching(h, t) {
-        return;
+        return *t;
     }
 
-    let r = match (h, t) {
-        (h, t) if t.x == h.x && h.y < t.y => Coord { x: t.x, y: t.y - 1 },
-        (h, t) if t.x < h.x && h.y < t.y => Coord {
-            x: t.x + 1,
-            y: t.y - 1,
-        },
-        (h, t) if t.x < h.x && h.y == t.y => Coord { x: t.x + 1, y: t.y },
-        (h, t) if t.x < h.x && h.y > t.y => Coord {
-            x: t.x + 1,
-            y: t.y + 1,
-        },
-        (h, t) if t.x == h.x && h.y > t.y => Coord { x: t.x, y: t.y + 1 },
-        (h, t) if t.x > h.x && h.y > t.y => Coord {
-            x: t.x - 1,
-            y: t.y + 1,
-        },
-        (h, t) if t.x > h.x && h.y == t.y => Coord { x: t.x - 1, y: t.y },
-        (h, t) if t.x > h.x && h.y < t.y => Coord {
-            x: t.x - 1,
-            y: t.y - 1,
-        },
+    match (h, t) {
+        (h, t) if t.0 == h.0 && h.1 < t.1 => (t.0, t.1 - 1),
+        (h, t) if t.0 < h.0 && h.1 < t.1 => (t.0 + 1, t.1 - 1),
+        (h, t) if t.0 < h.0 && h.1 == t.1 => (t.0 + 1, t.1),
+        (h, t) if t.0 < h.0 && h.1 > t.1 => (t.0 + 1, t.1 + 1),
+        (h, t) if t.0 == h.0 && h.1 > t.1 => (t.0, t.1 + 1),
+        (h, t) if t.0 > h.0 && h.1 > t.1 => (t.0 - 1, t.1 + 1),
+        (h, t) if t.0 > h.0 && h.1 == t.1 => (t.0 - 1, t.1),
+        (h, t) if t.0 > h.0 && h.1 < t.1 => (t.0 - 1, t.1 - 1),
         _ => panic!(),
-    };
-
-    rope[i] = r;
+    }
 }
 
-fn is_touching(c1: &Coord, c2: &Coord) -> bool {
-    let dist_x = (c1.x - c2.x).abs();
-    let dist_y = (c1.y - c2.y).abs();
+fn is_touching(c1: &(i32, i32), c2: &(i32, i32)) -> bool {
+    let dist_x = (c1.0 - c2.0).abs();
+    let dist_y = (c1.1 - c2.1).abs();
     if dist_x >= 2 || dist_y >= 2 {
         false
     } else {
