08857d3de6eff098f6b6a8a43fbe3f64decee913
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-02-04 19:51:56 +0100

    optimize day 16 (use fnv hasher)

--- src/day16.rs
+++ src/day16.rs
@@ -1,10 +1,11 @@
-use std::collections::HashMap;
-
+use fnv::FnvHashMap; // faster hashmap
 use lazy_regex::regex_captures;
 
 type Bitmask = i128;
 type Valve = i32;
+type ValvePair = i32;
 
+// Map valve names to integers
 fn str_to_valve(s: &str) -> Valve {
     let mut chars = s.chars();
     let c1 = chars.next().unwrap() as i32;
@@ -12,12 +13,17 @@ fn str_to_valve(s: &str) -> Valve {
     c1 << 8 | c2
 }
 
+fn valve_pair(v1: &Valve, v2: &Valve) -> ValvePair {
+    v1 << 16 | v2
+}
+
 pub fn solve() -> (i64, i64) {
     let buf = include_bytes!("../inputs/input16.txt");
-    let mut graph: HashMap<Valve, Vec<Valve>> = HashMap::new();
-    let mut flows: HashMap<Valve, i64> = HashMap::new();
-    let mut valves: Vec<String> = Vec::new();
-    let mut indices: HashMap<String, Bitmask> = HashMap::new();
+    let mut graph: FnvHashMap<Valve, Vec<Valve>> = FnvHashMap::default();
+    let mut flows: FnvHashMap<Valve, i64> = FnvHashMap::default();
+    let mut valves: Vec<Valve> = Vec::new();
+    let mut indices: FnvHashMap<Valve, Bitmask> = FnvHashMap::default();
+    let mut dists: FnvHashMap<ValvePair, i64> = FnvHashMap::default();
 
     for line in String::from_utf8_lossy(buf).trim().split("\n") {
         let (_, valve, flow_rate, leads_to) = regex_captures!(
@@ -27,14 +33,18 @@ pub fn solve() -> (i64, i64) {
         .unwrap();
 
         graph.insert(
-            valve.to_string(),
-            leads_to.split(',').map(|s| s.trim().to_string()).collect(),
+            str_to_valve(valve),
+            leads_to
+                .split(',')
+                .map(|s| str_to_valve(s.trim()))
+                .collect(),
         );
-        valves.push(valve.to_string());
+
+        valves.push(str_to_valve(valve));
 
         let fr = flow_rate.parse::<i64>().unwrap();
         if fr > 0 {
-            flows.insert(valve.to_string(), fr);
+            flows.insert(str_to_valve(valve), fr);
         }
     }
 
@@ -45,10 +55,9 @@ pub fn solve() -> (i64, i64) {
     }
 
     // Distances between any pair of nodes (Floyd-Warshall)
-    let mut dists: HashMap<(String, String), i64> = HashMap::new();
     for v in &valves {
         for l in &valves {
-            let pair = (v.clone(), l.clone());
+            let pair = valve_pair(v, l);
             if graph.get(v).unwrap().contains(l) {
                 dists.insert(pair, 1);
             } else {
@@ -60,24 +69,21 @@ pub fn solve() -> (i64, i64) {
     for k in &valves {
         for i in &valves {
             for j in &valves {
-                let dist_ij = *dists.get(&(i.to_string(), j.to_string())).unwrap();
-                let dist_ik = *dists.get(&(i.to_string(), k.to_string())).unwrap();
-                let dist_kj = *dists.get(&(k.to_string(), j.to_string())).unwrap();
-                dists.insert(
-                    (i.to_string(), j.to_string()),
-                    dist_ij.min(dist_ik + dist_kj),
-                );
+                let dist_ij = *dists.get(&valve_pair(i, j)).unwrap();
+                let dist_ik = *dists.get(&valve_pair(i, k)).unwrap();
+                let dist_kj = *dists.get(&valve_pair(k, j)).unwrap();
+                dists.insert(valve_pair(i, j), dist_ij.min(dist_ik + dist_kj));
             }
         }
     }
 
-    let mut answers_p1 = HashMap::new();
-    let start: String = "AA".to_string();
-    visit(&start, 30, 0, 0, &mut answers_p1, &flows, &dists, &indices);
+    let mut answers_p1 = FnvHashMap::default();
+    let start = str_to_valve("AA");
+    visit(start, 30, 0, 0, &mut answers_p1, &flows, &dists, &indices);
     let p1 = *answers_p1.values().max().unwrap();
 
-    let mut answers_p2 = HashMap::new();
-    visit(&start, 26, 0, 0, &mut answers_p2, &flows, &dists, &indices);
+    let mut answers_p2 = FnvHashMap::default();
+    visit(start, 26, 0, 0, &mut answers_p2, &flows, &dists, &indices);
     let mut p2 = 0;
     for (k1, v1) in &answers_p2 {
         for (k2, v2) in &answers_p2 {
@@ -93,14 +99,14 @@ pub fn solve() -> (i64, i64) {
     (p1, p2)
 }
 fn visit(
-    valve: &String,
+    valve: Valve,
     minutes: i64,
     bitmask: Bitmask,
     pressure: i64,
-    answer: &mut HashMap<Bitmask, i64>,
-    flows: &HashMap<String, i64>,
-    dists: &HashMap<(String, String), i64>,
-    indices: &HashMap<String, Bitmask>,
+    answer: &mut FnvHashMap<Bitmask, i64>,
+    flows: &FnvHashMap<Valve, i64>,
+    dists: &FnvHashMap<ValvePair, i64>,
+    indices: &FnvHashMap<Valve, Bitmask>,
 ) {
     answer
         .entry(bitmask)
@@ -112,13 +118,13 @@ fn visit(
         .or_insert(pressure);
 
     for (valve2, flow) in flows {
-        let dist = dists.get(&(valve.to_string(), valve2.to_string())).unwrap();
+        let dist = dists.get(&valve_pair(&valve, valve2)).unwrap();
         let remaining_minutes = minutes - dist - 1;
         if remaining_minutes > 0 {
             let i: Bitmask = *indices.get(valve2).unwrap();
             if bitmask & i == 0 {
                 visit(
-                    &valve2,
+                    *valve2,
                     remaining_minutes,
                     bitmask | i,
                     pressure + flow * remaining_minutes,
