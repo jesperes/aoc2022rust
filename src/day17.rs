686142e6946af13395a4225528a367bb3c19d548
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-03-31 17:04:14 +0200

    wip

--- src/day17.rs
+++ src/day17.rs
@@ -1,17 +1,22 @@
+use std::collections::HashMap;
+
+use num::integer::div_floor;
+
 type ChamberCoord = (i32, i32);
 type RockCoord = (i32, i32);
 type Delta = (i32, i32);
 
 #[derive(Copy, Clone, Debug)]
 struct Jet {
+    index: i32,
     dx: i32,
 }
 
 impl Jet {
-    fn new(c: char) -> Self {
+    fn new(index: i32, c: char) -> Self {
         match c {
-            '>' => Jet { dx: 1 },
-            '<' => Jet { dx: -1 },
+            '>' => Jet { index, dx: 1 },
+            '<' => Jet { index, dx: -1 },
             _ => panic!(),
         }
     }
@@ -84,7 +89,7 @@ impl Chamber {
             shape_index: 0,
             height: 0,
             // num_dropped_rocks: 0,
-            tower: vec![0; 4096],
+            tower: vec![0; 16 * 1024],
         }
     }
 
@@ -96,12 +101,16 @@ impl Chamber {
 
     fn next_jet(&mut self) -> Jet {
         let mut c = self.jets[self.jet_index];
+        let mut index = self.jet_index;
+
         if c as char == '\n' {
             self.jet_index = 0;
             c = self.jets[0];
+            index = 0;
         }
+
         self.jet_index += 1;
-        return Jet::new(c as char);
+        return Jet::new(index as i32, c as char);
     }
 
     fn next_rock(&mut self) -> Rock {
@@ -192,21 +201,77 @@ impl Chamber {
     }
 }
 
+struct CycleTracker {
+    states: HashMap<Vec<i32>, (i64, i32)>,
+}
+
+impl CycleTracker {
+    fn new() -> Self {
+        CycleTracker {
+            states: HashMap::new(),
+        }
+    }
+
+    fn add_state(&mut self, jet: &Jet, chamber: &Chamber, rock_num: i64) -> Option<(i64, i32)> {
+        let mut state: Vec<i32> = Vec::new();
+        state.push(jet.index);
+        for i in 1..20 {
+            let y = chamber.height - i;
+            if y < 0 {
+                break;
+            }
+            state.push(chamber.tower[y as usize] as i32);
+        }
+
+        println!("State: {:?}", state);
+
+        if let Some((rock_num, height)) = self.states.get(&state) {
+            return Some((*rock_num, *height));
+        } else {
+            self.states.insert(state, (rock_num, chamber.height));
+            return None;
+        }
+    }
+}
+
 pub fn solve() -> (i32, i64) {
     let jets = include_bytes!("../inputs/input17.txt");
     // let jets = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>\n".as_bytes();
     let mut chamber = Chamber::new(jets);
     const DEBUG1: bool = false;
     const DEBUG: bool = false;
+    let mut cycle_tracker = CycleTracker::new();
 
-    for _ in 0..2022 {
+    let mut p1: i32 = 0;
+    let mut p2: i64 = 0;
+    let p1_limit = 2022;
+
+    // When we discover a cycle, we will compute the length of the cycle, and reduce
+    // the number of iterations we need to do.
+    let mut p2_limit: i64 = 1_000_000_000_000;
+    let mut rock_num: i64 = 0;
+
+    let mut height_of_all_cycles: i64 = 0;
+
+    'outer: loop {
+        println!("Rock num: {rock_num}");
+
+        if rock_num == p1_limit {
+            p1 = chamber.height;
+        }
+
+        if rock_num == p2_limit {
+            p2 = chamber.height as i64 + height_of_all_cycles;
+            break 'outer;
+        }
+
         let mut rock = chamber.next_rock();
         if DEBUG1 {
             println!("A new rock begins falling:");
             chamber.print(Some(&rock));
         }
 
-        loop {
+        'inner: loop {
             let jet = chamber.next_jet();
 
             if chamber.maybe_move_sideways(&mut rock, jet) {
@@ -236,15 +301,51 @@ pub fn solve() -> (i32, i64) {
                 }
             } else {
                 chamber.add_to_tower(rock);
+
+                if let Some((start, height)) = cycle_tracker.add_state(&jet, &chamber, rock_num) {
+                    let rock_num_at_cycle_start = start as i64;
+                    let height_at_cycle_start = height as i64;
+                    let cycle_height = chamber.height as i64 - height_at_cycle_start;
+                    let rocks_per_cycle = rock_num as i64 - rock_num_at_cycle_start;
+
+                    println!("Found repeating state:");
+                    println!(
+                        "First occurrence after dropping rock number {} (height {})",
+                        rock_num_at_cycle_start, height_at_cycle_start
+                    );
+                    println!(
+                        "Second occurence after dropping rock number {} (height {})",
+                        rock_num, chamber.height
+                    );
+                    println!(
+                        "Cycle length: {}",
+                        rock_num as i64 - rock_num_at_cycle_start
+                    );
+
+                    let p2_num_rocks_to_drop: i64 = 1_000_000_000_000;
+                    let number_of_rocks_left_to_drop_when_cycles_start =
+                        p2_num_rocks_to_drop - rock_num_at_cycle_start;
+                    let num_cycles =
+                        number_of_rocks_left_to_drop_when_cycles_start.div_floor(rocks_per_cycle);
+
+                    let rocks_part_of_cycles = num_cycles * rocks_per_cycle;
+                    height_of_all_cycles = cycle_height * num_cycles;
+
+                    println!("Reducing p2_limit by {}", rocks_part_of_cycles);
+                    p2_limit -= rocks_part_of_cycles;
+                }
+
                 if DEBUG {
                     println!("Rock falls 1 unit, causing it to come to rest:");
                     chamber.print(None);
                 }
-                break;
+
+                break 'inner;
             }
+
+            rock_num += 1;
         }
     }
 
-    println!("Height: {}", chamber.height);
-    (0, 0)
+    (p1, p2)
 }
