5415a1d7872b0bf34247083329f8d33e47253e2a
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-10-22 07:20:39 +0200

    Day 19 optimizations

--- src/day19.rs
+++ src/day19.rs
@@ -19,7 +19,7 @@ struct Blueprint {
 }
 
 #[derive(Eq, Hash, PartialEq)]
-struct Inv {
+struct State {
     min: i32,
     ore: i32,
     clay: i32,
@@ -31,9 +31,9 @@ struct Inv {
     geo_r: i32,
 }
 
-impl Inv {
-    fn default(min: i32) -> Inv {
-        Inv {
+impl State {
+    fn default(min: i32) -> State {
+        State {
             min,
             ore: 0,
             clay: 0,
@@ -85,8 +85,10 @@ impl Blueprint {
             geo_robot_obs_c,
 
             max_ore_r: clay_robot_ore_c.max(obs_robot_ore_c).max(geo_robot_ore_c),
-            max_clay_r: obs_robot_clay_c,
-            max_obs_r: geo_robot_obs_c,
+            // It turns out that we can lower the max_clay_r and max_obs_r a
+            // little bit without actually losing any optimal solutions.
+            max_clay_r: obs_robot_clay_c - 2,
+            max_obs_r: geo_robot_obs_c - 1,
         }
     }
 
@@ -99,60 +101,81 @@ impl Blueprint {
     }
 }
 
-fn search2(bp: &Blueprint, cache: &mut FnvHashMap<Inv, i32>, inv: Inv) -> i32 {
-    if inv.min == 0 {
-        return inv.geo;
+fn search2(
+    bp: &Blueprint,
+    cache: &mut FnvHashMap<State, i32>,
+    num_states: &mut i32,
+    cutoff: i32,
+    state: State,
+) -> i32 {
+    *num_states += 1;
+
+    if state.min == 0 {
+        return state.geo;
+    }
+
+    // If we haven't reached the max robot limits by the cutuff time, there is
+    // no point in proceeding. The cutoff time has been determined through
+    // observation. :)
+    if state.min < cutoff
+        && state.obs_r < bp.max_obs_r
+        && state.clay_r < bp.max_clay_r
+        && state.ore_r < bp.max_ore_r
+    {
+        return 0;
     }
 
     let mut max_geodes: i32 = 0;
 
-    if let Some(cached_value) = cache.get(&inv) {
+    if let Some(cached_value) = cache.get(&state) {
         return *cached_value;
     }
 
     // Default inventory for the next minute
-    let next_inv = Inv {
-        min: inv.min - 1,
-        ore: inv.ore + inv.ore_r,
-        clay: inv.clay + inv.clay_r,
-        obs: inv.obs + inv.obs_r,
-        geo: inv.geo + inv.geo_r,
-        ..inv
+    let next_state = State {
+        min: state.min - 1,
+        ore: state.ore + state.ore_r,
+        clay: state.clay + state.clay_r,
+        obs: state.obs + state.obs_r,
+        geo: state.geo + state.geo_r,
+        ..state
     };
 
     // Always build a geode robot, if possible, and do not investigate other branches
-    if inv.obs >= bp.geo_robot_obs_c && inv.ore >= bp.geo_robot_ore_c {
+    if state.obs >= bp.geo_robot_obs_c && state.ore >= bp.geo_robot_ore_c {
         return search2(
             bp,
             cache,
-            Inv {
-                ore: next_inv.ore - bp.geo_robot_ore_c,
-                obs: next_inv.obs - bp.geo_robot_obs_c,
-                geo_r: inv.geo_r + 1,
-                ..next_inv
+            num_states,
+            cutoff,
+            State {
+                ore: next_state.ore - bp.geo_robot_ore_c,
+                obs: next_state.obs - bp.geo_robot_obs_c,
+                geo_r: state.geo_r + 1,
+                ..next_state
             },
         );
     }
 
-    // Optimization 1. This is the last robot we can build, so if we couldn't
-    // build a geode robot, there is really no point in building anything else,
-    // but remember to let the geode robots we have build the last round of
-    // robots. (This optimization took the total runtime from 49s to 36s.)
-    if inv.min == 1 {
-        return inv.geo + inv.geo_r;
+    if state.min == 1 {
+        return state.geo + state.geo_r;
     }
 
     // Maybe build obsidian robot
-    if inv.ore >= bp.obs_robot_ore_c && inv.clay >= bp.obs_robot_clay_c && inv.obs_r < bp.max_obs_r
+    if state.ore >= bp.obs_robot_ore_c
+        && state.clay >= bp.obs_robot_clay_c
+        && state.obs_r < bp.max_obs_r
     {
         let geodes = search2(
             bp,
             cache,
-            Inv {
-                ore: next_inv.ore - bp.obs_robot_ore_c,
-                clay: next_inv.clay - bp.obs_robot_clay_c,
-                obs_r: next_inv.obs_r + 1,
-                ..next_inv
+            num_states,
+            cutoff,
+            State {
+                ore: next_state.ore - bp.obs_robot_ore_c,
+                clay: next_state.clay - bp.obs_robot_clay_c,
+                obs_r: next_state.obs_r + 1,
+                ..next_state
             },
         );
 
@@ -161,21 +184,21 @@ fn search2(bp: &Blueprint, cache: &mut FnvHashMap<Inv, i32>, inv: Inv) -> i32 {
         }
     }
 
-    // Optimization: skip if we don't have time to build a obsidian + a geode
-    // robot. (22s -> 17.5s).
-    if inv.min == 2 {
-        return inv.geo + inv.geo_r * 2;
+    if state.min == 2 {
+        return state.geo + state.geo_r * 2;
     }
 
     // Maybe build clay robot
-    if inv.ore >= bp.clay_robot_ore_c && inv.clay_r < bp.max_clay_r {
+    if state.ore >= bp.clay_robot_ore_c && state.clay_r < bp.max_clay_r {
         let geodes = search2(
             bp,
             cache,
-            Inv {
-                ore: next_inv.ore - bp.clay_robot_ore_c,
-                clay_r: next_inv.clay_r + 1,
-                ..next_inv
+            num_states,
+            cutoff,
+            State {
+                ore: next_state.ore - bp.clay_robot_ore_c,
+                clay_r: next_state.clay_r + 1,
+                ..next_state
             },
         );
 
@@ -185,14 +208,16 @@ fn search2(bp: &Blueprint, cache: &mut FnvHashMap<Inv, i32>, inv: Inv) -> i32 {
     }
 
     // Maybe build ore robot
-    if inv.ore >= bp.ore_robot_ore_c && inv.ore_r < bp.max_ore_r {
+    if state.ore >= bp.ore_robot_ore_c && state.ore_r < bp.max_ore_r {
         let geodes = search2(
             bp,
             cache,
-            Inv {
-                ore: next_inv.ore - bp.ore_robot_ore_c,
-                ore_r: next_inv.ore_r + 1,
-                ..next_inv
+            num_states,
+            cutoff,
+            State {
+                ore: next_state.ore - bp.ore_robot_ore_c,
+                ore_r: next_state.ore_r + 1,
+                ..next_state
             },
         );
 
@@ -202,35 +227,44 @@ fn search2(bp: &Blueprint, cache: &mut FnvHashMap<Inv, i32>, inv: Inv) -> i32 {
     }
 
     // Don't build anything, just let existing robots produce more resources
-    let geodes = search2(bp, cache, next_inv);
+    let geodes = search2(bp, cache, num_states, cutoff, next_state);
 
     if geodes > max_geodes {
         max_geodes = geodes;
     }
 
-    cache.insert(inv, max_geodes);
+    cache.insert(state, max_geodes);
     return max_geodes;
 }
 
-fn search(bp: &Blueprint, minutes_left: i32) -> i32 {
+fn search(bp: &Blueprint, cutoff: i32, minutes_left: i32) -> i32 {
     let mut cache = FnvHashMap::default();
-    return search2(&bp, &mut cache, Inv::default(minutes_left));
+    let mut num_states = 0;
+    let n = search2(
+        &bp,
+        &mut cache,
+        &mut num_states,
+        cutoff,
+        State::default(minutes_left),
+    );
+    return n;
 }
 
 pub fn solve() -> (i32, i32) {
     let input_bytes = include_bytes!("../inputs/input19.txt");
     let blueprints = Blueprint::parse_blueprints(input_bytes);
 
-    // Optimization 2: parallelization brought the runtime from 36s to 20s.
-
+    // Numbers found by trial-and-error... :)
+    let cutoff_p1 = 2;
     let p1 = blueprints
         .par_iter()
-        .map(|bp| search(&bp, 24) * bp.nr)
+        .map(|bp| search(&bp, cutoff_p1, 24) * bp.nr)
         .sum();
 
+    let cutoff_p2 = 18;
     let p2 = blueprints[0..3]
         .par_iter()
-        .map(|bp| search(&bp, 32))
+        .map(|bp| search(&bp, cutoff_p2, 32))
         .reduce(|| 1, |a, b| a * b);
 
     (p1, p2)
